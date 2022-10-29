6b72fe905e118d8a2979fb91dbd3ead60731f7ab
Adrian Fernandez <54385479+xtuu@users.noreply.github.com>
2022-10-28 21:02:38 -0400

    Templates

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styleUrls: ['./basicos.component.css']
+
 })
 export class BasicosComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  guardar(miFormulario: NgForm) {
+    console.log(miFormulario)
   }
 
 }
