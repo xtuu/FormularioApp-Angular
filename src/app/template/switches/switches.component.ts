03758c6b6a036401f0f4ee178ec0e7cab4c60319
Adrian Fernandez <54385479+xtuu@users.noreply.github.com>
2022-11-07 13:07:00 -0400

    [NEW]

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styleUrls: ['./switches.component.css']
+
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  persona = {
+    genero: 'F',
+    notificaciones: true
   }
+
+  terminosYConcidiciones: boolean = false
+
 
 }
