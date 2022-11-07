51c66d799942bdbeb5cdb5d93c17f143f5c0755b
Adrian Fernandez <54385479+xtuu@users.noreply.github.com>
2022-11-07 08:58:22 -0400

    [NEW] aplicacion de directivas

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -7,13 +7,15 @@ import { TemplateRoutingModule } from './template-routing.module';
 import { BasicosComponent } from './basicos/basicos.component';
 import { DinamicosComponent } from './dinamicos/dinamicos.component';
 import { SwitchesComponent } from './switches/switches.component';
+import { CustomMinDirective } from './directives/custom-min.directive';
 
 
 @NgModule({
   declarations: [
     BasicosComponent,
     DinamicosComponent,
-    SwitchesComponent
+    SwitchesComponent,
+    CustomMinDirective
   ],
   imports: [
     CommonModule,
