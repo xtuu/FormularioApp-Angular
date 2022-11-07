e54f04338aad6aba27f8870de50ea80f5445fffd
Adrian Fernandez <54385479+xtuu@users.noreply.github.com>
2022-11-07 15:52:01 -0400

    [NEW] reactives form

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
   // styleUrls: ['./basicos.component.css']
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre: new FormControl('RT 4080ti'),
+  //   precio: new FormControl(1500),
+  //   existencias: new FormControl(5),
+  // })
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['RTX 4080ti', [Validators.required, Validators.minLength(3)]],
+    precio: [0, [Validators.min(0), Validators.required]],
+    existencias: [0, [Validators.min(0), Validators.required]],
+
+  })
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+
 
 }
