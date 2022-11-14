1462614439a0ff369b4eb6ff915c9b732c1b44fa
Adrian Fernandez <54385479+xtuu@users.noreply.github.com>
2022-11-14 19:43:29 -0400

    [UPDATE]

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+
+  //temporal
+  nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)]]
+  })
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+  }
+
+
+
+  campoNoValido(campo: string) {
+    return this.miFormulario.get(campo)?.invalid
+      && this.miFormulario.get(campo)?.touched
+  }
+
+  submitFormulario() {
+    console.log(this.miFormulario.value)
+
+    this.miFormulario.markAllAsTouched()
   }
 
 }
