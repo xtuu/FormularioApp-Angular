69a7399f761dd932d4194209eb93c2ee7da87c27
Adrian Fernandez <54385479+xtuu@users.noreply.github.com>
2022-11-14 18:37:31 -0400

    [UPDATE]

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 
@@ -9,7 +10,7 @@ const routes: Routes = [
     children: [
       { path: 'login', component: LoginComponent },
       { path: 'registro', component: RegistroComponent },
-      { path: '**', redirectTo: 'login' },
+      { path: '**', redirectTo: 'registro' },
     ]
   }
 ];
--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  // styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
 
