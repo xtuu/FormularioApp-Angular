637ab0dae2d9204bee75fbc38b497c241849598e
Adrian Fernandez <54385479+xtuu@users.noreply.github.com>
2022-11-15 08:48:49 -0400

    [UPDATE]

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailValidatorService } from 'src/app/shared/validator/email-validator.service';
 
 // import { emailPattern, nombreApellidoPattern, noPuedeSerStrider } from 'src/app/shared/validator/validaciones';
 import { ValidatorService } from 'src/app/shared/validator/validator.service';
@@ -17,7 +18,7 @@ export class RegistroComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [Validators.required, Validators.pattern(this.vs.nombreApellidoPattern)]],
-    email: ['', [Validators.required, Validators.pattern(this.vs.emailPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.vs.emailPattern)], [this.emailValidator]],
     username: ['', [Validators.required, this.vs.noPuedeSerStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     confirPassword: ['', [Validators.required]],
@@ -27,14 +28,15 @@ export class RegistroComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private vs: ValidatorService
+    private vs: ValidatorService,
+    private emailValidator: EmailValidatorService
   ) { }
 
   ngOnInit(): void {
 
     this.miFormulario.reset({
       nombre: 'Adrian Fernandez',
-      email: 'adrian@ejemplo.com',
+      email: 'test1@test.com',
       username: 'xtuu'
     })
   }
--- src/app/shared/validator/email-validator.service.ts
+++ src/app/shared/validator/email-validator.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -16,8 +18,15 @@ export class EmailValidatorService implements AsyncValidator {
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
 
     const email = control.value
-
+    console.log(email)
     return this.http.get<any[]>(`http://localhost:3000/usuarios?q=${email}`)
+      .pipe(
+        map(resp => {
+          return (resp.length === 0)
+            ? null
+            : { emailTomado: true }
+        })
+      )
 
   }
 
